be9c21cbb5d566ed57dadbf9eca1cfc238f7c075
phoenixg <306761352@qq.com>
2013-10-22 11:23:48 +0800

    aaa

--- schools/tongji1.php
+++ schools/tongji1.php
@@ -4,22 +4,21 @@
 
 set_time_limit(0);
 
-header("Content-type: text/html; charset=utf-8");
+header("Content-type: text/html; charset=gbk");
 
 include '../libs/phpquery/phpQuery-onefile.php';
+define('BASE_URL', 'http://news.tongji.edu.cn/');
+
 $html = file_get_contents( 'http://news.tongji.edu.cn/classid-21.html');
 
-phpQuery::newDocumentHTML( $html , 'utf-8' );
+phpQuery::newDocumentHTML( $html , 'gbk' );
 
-//$table = pq('.news_list > li.li_black > a');
 $table = pq('#content .news_list a');
 
 $result = array();
 foreach ($table as $index => $value) {
     $v = pq($value);
 
-echo ($v);die;
-/* // todo
     $lecUrl = BASE_URL . $v->attr('href');
     $result[$index] = array(
         'lecUrl' => $lecUrl,
@@ -28,12 +27,14 @@ echo ($v);die;
     // parse the single lecture page
     phpQuery::newDocumentHTML( file_get_contents($lecUrl) , 'gbk' );
 
-    $result[$index]['lecTitle'] = pq('#lecTitle')->html();
+    $result[$index]['lecTitle'] = pq('.news_title')->html();
+    // todo
+    /*
     $result[$index]['lecTime'] = pq('#lecBeginTime')->html();
     $result[$index]['lecSpeaker'] = pq('#lecSpeaker')->html();
     $result[$index]['lecAddress'] = pq('#lecAddress')->html();
     $result[$index]['lecContent'] = mb_convert_encoding(pq('#lecContent')->text(), 'GBK', 'UTF-8');
-*/
+    */
+    print_r($result);die;
 }
 
-echo 111;
