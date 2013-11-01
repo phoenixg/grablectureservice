2366ade32aefecb2d8e5770d3f7ee27fc7062148
linode phx <gopher.huang@gmail.com>
2013-11-01 06:23:39 -0400

    add api

---
+++ api/api.php
@@ -0,0 +1,24 @@
+<?php
+
+// 输出格式
+return array(
+	'ecnu1' => array(
+		'lecUrl' => 'http://www.baidu.com/',
+		'lecTitle' => '孔子的思想',
+		'lecTime' => '2013-11-07 10:00',
+		'lecSpeaker' => 'Dr. Wu',
+		'lecAddress' => '闵行信息楼111金碧辉煌厅',
+		'lecContent' => '孔子好厉害哦',
+		'longitude' => '11.11111111',
+		'latitude' => '22.22222222',
+		'subSchool' => '闵行校区',
+		'tags' => array(1, 3, 4),
+		
+	),
+
+
+
+
+
+
+);
